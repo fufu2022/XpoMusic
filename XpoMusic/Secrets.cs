da705b6f7431068c717926f494ce4616423c4b66
fufu2022 <111653524+fufu2022@users.noreply.github.com>
2022-12-29 21:58:26 +0800

    Create Secrets.cs

---
+++ XpoMusic/Secrets.cs
@@ -0,0 +1,10 @@
+namespace XpoMusic
+{
+    internal static class Secrets
+    {
+        internal static readonly string SpotifyClientId = "";
+        internal static readonly string SpotifyClientSecret = "";
+        internal static readonly string GoogleAnalyticsTrackerId = "";
+        internal static readonly string AppCenterId = "";
+    }
+}
